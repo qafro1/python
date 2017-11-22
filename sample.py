3a22018e476e08f96e7af74ef352f239fd615d92
Gokere <gokere@williamhill.co.uk>
2017-11-22 14:58:10 +0000

    added timestamp to archive

--- sample.py
+++ sample.py
@@ -1,15 +1,19 @@
 #!usr/bin/python
-#This python script archive files gz in a source directory to a target directory as backup
+#This python script will archive timestamp files gz in a source directory to a target directory as backup
 
 import tarfile
 import glob
 import os
+from datetime import datetime
+import time
 
 #input the source directory
 src = "path"
 #archive name of the target path directory
 archive_name = "path"
+#timestamp archive_name
+archive_name_time = datetime.now().strftime(archive_name + '_%H_%M_%d_%m_%Y.gz')
 # write to latest_files.tar.gz
-with tarfile.open(archive_name, "w:gz") as tar:
+with tarfile.open(archive_name_time, "w:gz") as tar:
   for name in glob.glob(os.path.join(src,"*")):
       tar.add(name,arcname=os.path.basename(name),exclude=os.path.isdir)
