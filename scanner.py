d0fc763e0eb3e9334eb315126ab4a64e0301a021
Gokere <gokere@williamhill.co.uk>
2017-12-05 14:30:31 +0000

    First commit

--- scanner.py
+++ scanner.py
@@ -3,28 +3,25 @@
 import os
 import os.path
 import time
+import glob
 
-
-#Check if files in directory and Last modified
+#Check if files in directory and Last modified or is inaccessible
 def files(path):
-  if os.path.isfile(path):
-    return False
+  if  glob.glob(filename):
+    return " Files in directory.\n" + "Last modified: %s" % time.ctime(os.path.getmtime(path))
   else:
-    return "Files in directory. Last modified: %s" % time.ctime(os.path.getmtime(path))
+    return "\n" + "NO files"
 
 # the time of last access of path.
 def atime(path):
     if os.path.getatime(path):
-        return "The time of last access of path: %s" % time.ctime(os.path.getatime(path)) + "\n" + path
-# if the file does not exist by the numbers of files or is inaccessible
-def sizeDir(path):
-    if os.path.getsize(path):
-         dirs = os.listdir(path)
-         if len(dirs) != 0:
-           return files(path)
-    else:
-        return IOError
+        return "The time of last access of path: %s" % time.ctime(os.path.getatime(path)) + " \n" + path
+
+#Open log file
+file = open('path','w')
 #source directory of any files
-path = "path"
-print atime(path)
-print sizeDir(path)
+path = 'path'
+filename = 'path/*.gz'
+file.write(atime(path))
+file.write(files(path))
+file.close()
